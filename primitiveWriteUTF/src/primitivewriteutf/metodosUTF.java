79da3492404bde715ae6901e817410895a5629b1
David <dcounagogonzalez@danielcastelao.org>
2022-09-21 14:30:29 +0200

    Intentando resover algunos problemas

--- primitiveWriteUTF/src/primitivewriteutf/metodosUTF.java
+++ primitiveWriteUTF/src/primitivewriteutf/metodosUTF.java
@@ -5,6 +5,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -15,27 +16,30 @@ public class metodosUTF {
     
     public static void repCadeas() throws FileNotFoundException, IOException{
         
-        String exemplo = "o tempo está xélido \n";
+        String exemplo = "o tempo esta xelido \n";
         
         int cont= 0;
-        FileInputStream cadea = new FileInputStream("/home/dam2a/AD/texto3.txt");
-        BufferedInputStream cad = new BufferedInputStream(cadea);
-        DataInputStream dataCad = new DataInputStream(cad);
-        
         
         FileOutputStream cadea2 = new FileOutputStream ("/home/dam2a/AD/texto3.txt");
         BufferedOutputStream cad2 = new BufferedOutputStream(cadea2);
         DataOutputStream dataCad2 = new DataOutputStream(cad2);
-
+        
+        
+        FileInputStream cadea = new FileInputStream("/home/dam2a/AD/texto3.txt");
+        BufferedInputStream cad = new BufferedInputStream(cadea);
+        DataInputStream dataCad = new DataInputStream(cad);
         
         String cadCopi=null;
-        
+        File fich = new File("/home/dam2a/AD/texto3.txt");
         
         
         try{
         while (cont<3){
+            
+            System.out.println("escribindo a cadea: " + exemplo);
+            System.out.println(fich.length());
             dataCad2.writeUTF(exemplo);
-                       
+            
             cont ++;
             
         } 
@@ -49,8 +53,8 @@ public class metodosUTF {
         while(dataCad.available()!=0){
             cadCopi = dataCad.readUTF();
             
-            System.out.println("escribindo a cadea : " + cadCopi);
-            
+            System.out.println("lendo a cadea : " + cadCopi);
+            System.out.println(fich.length());
                 
         }
         }
