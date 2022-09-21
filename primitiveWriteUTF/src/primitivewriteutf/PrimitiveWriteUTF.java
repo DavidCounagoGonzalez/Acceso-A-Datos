8240a10111e83559a560874584ee3ed86402e6a8
David <dcounagogonzalez@danielcastelao.org>
2022-09-21 13:53:32 +0200

    Copia y lee el contenido

--- primitiveWriteUTF/src/primitivewriteutf/PrimitiveWriteUTF.java
+++ primitiveWriteUTF/src/primitivewriteutf/PrimitiveWriteUTF.java
@@ -1,11 +1,15 @@
 
 package primitivewriteutf;
 
+import java.io.IOException;
+
 
 public class PrimitiveWriteUTF {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        
+        metodosUTF.repCadeas();
         
     }
     
--- primitiveWriteUTF/src/primitivewriteutf/metodosUTF.java
+++ primitiveWriteUTF/src/primitivewriteutf/metodosUTF.java
@@ -15,34 +15,52 @@ public class metodosUTF {
     
     public static void repCadeas() throws FileNotFoundException, IOException{
         
+        String exemplo = "o tempo está xélido \n";
+        
         int cont= 0;
-        FileInputStream cadea = new FileInputStream("/home/dam2a/AD/cadea.txt");
+        FileInputStream cadea = new FileInputStream("/home/dam2a/AD/texto3.txt");
         BufferedInputStream cad = new BufferedInputStream(cadea);
         DataInputStream dataCad = new DataInputStream(cad);
-        String cadCopi = dataCad.readUTF();
+        
         
         FileOutputStream cadea2 = new FileOutputStream ("/home/dam2a/AD/texto3.txt");
         BufferedOutputStream cad2 = new BufferedOutputStream(cadea2);
         DataOutputStream dataCad2 = new DataOutputStream(cad2);
+
+        
+        String cadCopi=null;
+        
         
         
         try{
         while (cont<3){
-            // if (dataCad.available()=true){
-            dataCad2.writeUTF(cadCopi);
-            
+            dataCad2.writeUTF(exemplo);
+                       
             cont ++;
-            //} 
-        }
+            
+        } 
         }
         finally{
-            if(dataCad!=null){
-                dataCad.close();
-            }
             if(dataCad2!=null){
                 dataCad2.close();
             }
         }
+        try{
+        while(dataCad.available()!=0){
+            cadCopi = dataCad.readUTF();
+            
+            System.out.println("escribindo a cadea : " + cadCopi);
+            
+                
+        }
+        }
+        finally{
+            if(dataCad!=null){
+            dataCad.close();
+        }
+        }
+        
+        
         
     }
 }
